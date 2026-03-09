3fc3d55256758aa497d8945b1a86328677cbd65f
hariomgaikwad <gaikwadhariom46@gmail.com>
2026-03-09 23:19:39 +0530

    Print multiplication table

---
+++ Loops/problem4_print_multiplication_table.py
@@ -0,0 +1,15 @@
+# Write a Python program to print the 
+# multiplication table of a given number.
+# Given = 2
+
+# logic
+# stop: 11
+# run loop 10 times 
+# 2 * i (current number)
+
+n = 2
+
+for i in range(1, 11, 1):
+    product = n * i
+    print(product)
+
