f8705acb8829e0efae0d81219c77bfa232f5a452
hariomgaikwad <gaikwadhariom46@gmail.com>
2026-03-14 23:51:48 +0530

    Display Done message after for loop

---
+++ Loops/problem10_print_done_message.py
@@ -0,0 +1,11 @@
+# Display message after successful loop execution
+
+# logic
+# the for loop prints numbers from 0 to 4.
+# the else block runs after the loop finishes successfully.
+# it prints the message "Done!".
+
+for i in range(5):
+    print(i)
+else:
+    print("Done!")
