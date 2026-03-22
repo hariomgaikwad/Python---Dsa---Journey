4dbd50362a63e1d542184c9b22557f17e52d417b
hariomgaikwad <gaikwadhariom46@gmail.com>
2026-03-22 23:58:50 +0530

    Calculate cube of numbers from 1 to N

---
+++ Loops/problem16_print_cube_numbers.py
@@ -0,0 +1,11 @@
+# Calculate cube of numbers
+
+# logic
+# loop from 1 to input_number
+# For each number, calculate cube using i**3
+# Print number and its cube
+
+input_number = 6
+
+for i in range(1, input_number + 1):
+    print("Current Number is :", i, " and the cube is", i**3)
