c47e6de9093e00a8a0ca3c1ae8d36b327cce6323
hariomgaikwad <gaikwadhariom46@gmail.com>
2026-03-24 23:35:17 +0530

    Format output string using print separator

---
+++ Loops/Input_Output/problem1_format_output_string.py
@@ -0,0 +1,12 @@
+# Format output string
+
+# logic
+# Use print() with sep="**"
+# It adds ** between each string
+
+str1 = 'My'
+str2 = 'Name'
+str3 = 'Is'
+str4 = 'James'
+
+print(str1, str2, str3, str4, sep="**")
