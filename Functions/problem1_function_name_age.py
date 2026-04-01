25cdd5d3ae390cfa0021eba44d442d56b8d26d01
hariomgaikwad <gaikwadhariom46@gmail.com>
2026-04-01 20:59:55 +0530

    Create function with name and age parameters

---
+++ Functions/problem1_function_name_age.py
@@ -0,0 +1,14 @@
+# Function with two arguments
+
+# logic 
+# Define function using def
+# Pass name and age as parameters
+# Print values inside function
+# Call the function with arguments
+
+def display(name, age):
+    print("Name:", name)
+    print("Age:", age)
+
+# function call
+display("Rohan", 18)
