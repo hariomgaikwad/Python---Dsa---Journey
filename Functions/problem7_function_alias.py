7b0daa97afbe8bae5691a47862bddb06cc8cb4df
hariomgaikwad <gaikwadhariom46@gmail.com>
2026-04-04 20:50:16 +0530

    Assign new name to function and call it

---
+++ Functions/problem7_function_alias.py
@@ -0,0 +1,12 @@
+# Exercise 7: Assign new name to function
+
+
+def display_student(name, age):
+    print(name, age)
+
+# assign new name
+show_student = display_student
+
+# function calls
+display_student("Emma", 26)
+show_student("Emma", 26)
