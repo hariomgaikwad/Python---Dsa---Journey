5378f7d091b25feaafe21ce0d3e7af1c016f5412
hariomgaikwad <gaikwadhariom46@gmail.com>
2026-04-04 20:54:38 +0530

    Generate even numbers list using range

---
+++ Functions/problem8_even_numbers_list.py
@@ -0,0 +1,10 @@
+# Exercise 8: Generate even numbers between 4 and 30
+
+# logic 
+# Use range(4, 30, 2)
+# Start = 4, End = 30 (excluded), Step = 2
+# Convert to list using list()
+
+even_numbers = list(range(4, 30, 2))
+
+print(even_numbers)
