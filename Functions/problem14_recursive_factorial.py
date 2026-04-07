afe432323b99c5eb078be577515f4d480e05b6fd
hariomgaikwad <gaikwadhariom46@gmail.com>
2026-04-07 22:04:39 +0530

    Lambda function to square a number

---
+++ Functions/problem14_recursive_factorial.py
@@ -0,0 +1,10 @@
+# Exercise 14: Lambda function to square a number
+
+# logic
+# lambda x: x * x → takes input and returns square
+# Store in variable square
+# Call like normal function
+
+square = lambda x: x * x
+
+print(square(5))
