2ed42c1ef6422d9596f3a5d507107be8f7ec0944
hariomgaikwad <gaikwadhariom46@gmail.com>
2026-04-15 22:00:41 +0530

    Split string on hyphens

---
+++ Strings/problem7_split_string_hyphen.py
@@ -0,0 +1,13 @@
+# Exercise 7: Split string on hyphens
+
+# logic
+# Use split("-") to split string
+# Loop through result list
+# Print each part
+
+str1 = "Emma-is-a-data-scientist"
+
+words = str1.split("-")
+
+for word in words:
+    print(word)
