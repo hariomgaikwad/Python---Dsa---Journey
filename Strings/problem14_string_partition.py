7af60268ec0a992342b838232fa161363e0c2d3a
hariomgaikwad <gaikwadhariom46@gmail.com>
2026-04-19 22:48:33 +0530

    Split string using partition method

---
+++ Strings/problem14_string_partition.py
@@ -0,0 +1,11 @@
+# Exercise 15: String partition
+
+# logic
+# Use partition("@")
+# Returns tuple → (before, separator, after)
+
+str1 = "username@company.com"
+
+result = str1.partition("@")
+
+print(result)
