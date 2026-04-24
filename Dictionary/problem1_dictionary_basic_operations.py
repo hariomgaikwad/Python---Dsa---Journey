1a2230023bfe25a41c8c9cf907b1579a97da1654
hariomgaikwad <gaikwadhariom46@gmail.com>
2026-04-24 22:52:03 +0530

    Basic dictionary operations add update access

---
+++ Dictionary/problem1_dictionary_basic_operations.py
@@ -0,0 +1,18 @@
+# Exercise 1: Dictionary operations
+
+# logic
+# Update → student["age"] = 21
+# Add → student["city"] = "New York"
+# Access → student["name"]
+
+student = {"name": "Alice", "age": 20, "grade": "B"}
+
+# modify existing value
+student["age"] = 21
+
+# add new key-value pair
+student["city"] = "New York"
+
+# access specific key
+print(student)
+print("Name:", student["name"])
