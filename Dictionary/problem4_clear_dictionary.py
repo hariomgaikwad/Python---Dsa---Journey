7986eb996fbcc26f6f066da6d73c56ec367dcb5d
hariomgaikwad <gaikwadhariom46@gmail.com>
2026-04-25 23:02:27 +0530

    Clear all items from dictionary

---
+++ Dictionary/problem4_clear_dictionary.py
@@ -0,0 +1,11 @@
+# Exercise 4: Clear dictionary
+
+# logic
+# Use clear() to remove all items
+# Dictionary object remains, only data removed
+
+inventory = {"apples": 10, "bananas": 5, "oranges": 8}
+
+inventory.clear()
+
+print(inventory)
