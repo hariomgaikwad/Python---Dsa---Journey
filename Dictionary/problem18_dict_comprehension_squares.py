7ee793f504bf781d547d07b0a42701d96d9c4be4
hariomgaikwad <gaikwadhariom46@gmail.com>
2026-05-03 00:03:18 +0530

    Dictionary comprehension for squares

---
+++ Dictionary/problem18_dict_comprehension_squares.py
@@ -0,0 +1,10 @@
+# Exercise 18: Dictionary of squares using comprehension
+
+# logic
+# Loop inside {} using comprehension
+# Key → i
+# Value → i*i
+
+result = {i: i*i for i in range(1, 11)}
+
+print(result)
