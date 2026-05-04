aa8d5012f31d1d1e03a80f522e55915c8be44ed4
hariomgaikwad <gaikwadhariom46@gmail.com>
2026-05-04 23:06:30 +0530

    Clear all elements from set

---
+++ Sets/problem2_clear_set.py
@@ -0,0 +1,11 @@
+# Exercise 2: Clear all elements from set
+
+# logic
+# Use clear() to remove all elements
+# Set variable still exists (just empty)
+
+colors = {"red", "green", "blue"}
+
+colors.clear()
+
+print(colors)
