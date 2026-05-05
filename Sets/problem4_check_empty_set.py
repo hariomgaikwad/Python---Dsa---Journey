de8d072cf45d4c3a9fe69998d7eda655bb1d78b5
hariomgaikwad <gaikwadhariom46@gmail.com>
2026-05-05 22:27:18 +0530

    Check if set is empty

---
+++ Sets/problem4_check_empty_set.py
@@ -0,0 +1,12 @@
+# Exercise 4: Check if set is empty
+
+# logic
+# Empty set → False
+# Use if not data to check emptiness
+
+data = set()
+
+if not data:
+    print("The set is empty.")
+else:
+    print("The set is not empty.")
