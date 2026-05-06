57c42423f88de29df77832f70db574381b35e052
hariomgaikwad <gaikwadhariom46@gmail.com>
2026-05-06 23:09:53 +0530

    Union of two sets

---
+++ Sets/problem5_set_union.py
@@ -0,0 +1,12 @@
+# Exercise 5: Union of sets
+
+# logic
+# union() combines both sets
+# Duplicate values are removed automatically
+
+set_a = {1, 2, 3, 4}
+set_b = {3, 4, 5, 6}
+
+result = set_a.union(set_b)
+
+print("Union:", result)
