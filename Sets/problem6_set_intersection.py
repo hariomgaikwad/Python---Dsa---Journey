1e6e1e95dfa544c2c7e033fde31e1e9e18af02e0
hariomgaikwad <gaikwadhariom46@gmail.com>
2026-05-06 23:12:01 +0530

    Intersection of two sets

---
+++ Sets/problem6_set_intersection.py
@@ -0,0 +1,12 @@
+# Exercise 6: Intersection of sets
+
+# logic
+# intersection() finds common elements
+# Returns values present in both sets
+
+set_a = {1, 2, 3, 4}
+set_b = {3, 4, 5, 6}
+
+result = set_a.intersection(set_b)
+
+print("Intersection:", result)
