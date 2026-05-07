1d3aa254e384c7666d676d5ff50de113457da851
hariomgaikwad <gaikwadhariom46@gmail.com>
2026-05-07 23:45:34 +0530

    Symmetric difference of sets

---
+++ Sets/problem8_set_symmetric_difference.py
@@ -0,0 +1,12 @@
+# Exercise 8: Symmetric difference of sets
+
+# logic
+# symmetric_difference() finds non-common elements
+# Removes elements present in both sets
+
+set_a = {1, 2, 3, 4}
+set_b = {3, 4, 5, 6}
+
+result = set_a.symmetric_difference(set_b)
+
+print("Symmetric Difference:", result)
