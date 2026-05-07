24a1636a05e47057cd57348392201ae6928ec1a7
hariomgaikwad <gaikwadhariom46@gmail.com>
2026-05-07 23:42:51 +0530

    Difference of two sets

---
+++ Sets/problem7_set_difference.py
@@ -0,0 +1,12 @@
+# Exercise 7: Difference of sets
+
+# logic
+# difference() finds elements only in Set A
+# Removes common elements with Set B
+
+set_a = {1, 2, 3, 4}
+set_b = {3, 4, 5, 6}
+
+result = set_a.difference(set_b)
+
+print("Difference (A - B):", result)
