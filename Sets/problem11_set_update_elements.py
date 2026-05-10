3a85358e9778d37f1a5d6c443c94997d5c35408d
hariomgaikwad <gaikwadhariom46@gmail.com>
2026-05-10 23:36:43 +0530

    Add multiple elements to set using update

---
+++ Sets/problem11_set_update_elements.py
@@ -0,0 +1,12 @@
+# Exercise 11: Add multiple elements using update()
+
+# logic
+# update() adds multiple elements
+# Duplicate values are ignored automatically
+
+fruits = {"apple", "banana"}
+new_fruits = ["cherry", "mango", "apple"]
+
+fruits.update(new_fruits)
+
+print("Updated set:", fruits)
