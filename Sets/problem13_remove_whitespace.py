d65e6669152ab7f29a7562e45436afb735f1f153
hariomgaikwad <gaikwadhariom46@gmail.com>
2026-05-11 23:46:41 +0530

    Remove whitespace from string

---
+++ Sets/problem13_remove_whitespace.py
@@ -0,0 +1,11 @@
+# Exercise 13: Remove all spaces from string
+
+# logic
+# replace(" ", "") removes all spaces
+# Returns cleaned string
+
+str1 = " P y t h o n "
+
+result = str1.replace(" ", "")
+
+print(result)
