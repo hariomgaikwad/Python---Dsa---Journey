c362370afbe5e52b6b34d47b30d5dd565428c06c
hariomgaikwad <gaikwadhariom46@gmail.com>
2026-05-17 23:28:29 +0530

    Basic tuple operations

---
+++ Tuples/problem1_basic_tuple_operations.py
@@ -0,0 +1,12 @@
+# Exercise 1: Basic tuple operations
+
+# logic
+# fruits[0] → first element
+# fruits[-1] → last element
+# len() → tuple size
+
+fruits = ("apple", "banana", "cherry", "date")
+
+print("First element:", fruits[0])
+print("Last element:", fruits[-1])
+print("Length:", len(fruits))
