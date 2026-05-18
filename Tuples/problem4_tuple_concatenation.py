2e4916e2f4a25dead81db078533fea4b3cbd85de
hariomgaikwad <gaikwadhariom46@gmail.com>
2026-05-18 23:42:17 +0530

    Concatenate multiple tuples

---
+++ Tuples/problem4_tuple_concatenation.py
@@ -0,0 +1,13 @@
+# Exercise 4: Concatenate tuples
+
+# logic
+# + joins tuples together
+# Original tuples remain unchanged
+
+a = (1, 2)
+b = (3, 4)
+c = (5, 6)
+
+result = a + b + c
+
+print(result)
