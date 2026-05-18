e9a125db3b8cf90c9dbef28cc7c3e7f2a5dc5298
hariomgaikwad <gaikwadhariom46@gmail.com>
2026-05-18 23:41:10 +0530

    Repeat tuple using multiplication operator

---
+++ Tuples/problem3_tuple_repetition.py
@@ -0,0 +1,11 @@
+# Exercise 3: Repeat tuple using * operator
+
+# logic
+# * 3 repeats tuple three times
+# Works like repetition in strings/lists
+
+colors = ("red", "green")
+
+result = colors * 3
+
+print(result)
