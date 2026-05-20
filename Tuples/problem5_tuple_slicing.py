18ff3ebc2b1990ec6c9b2a2f8dcf44dc67c15bf5
hariomgaikwad <gaikwadhariom46@gmail.com>
2026-05-20 23:58:00 +0530

    Extract portion of tuple using slicing

---
+++ Tuples/problem5_tuple_slicing.py
@@ -0,0 +1,11 @@
+# Exercise 5: Slice a tuple
+
+# logic
+# Slicing syntax → [start:end]
+# Start index included, end index excluded
+
+numbers = (10, 20, 30, 40, 50, 60, 70)
+
+result = numbers[2:5]
+
+print(result)
