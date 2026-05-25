4f697b1babc8d76e9c7bb024d7aa528fb1b66b3f
hariomgaikwad <gaikwadhariom46@gmail.com>
2026-05-25 23:58:02 +0530

    Calculate sum of set elements using loop

---
+++ Tuples/problem10_sum_set_elements.py
@@ -0,0 +1,15 @@
+# Exercise 10: Calculate sum of set elements using loop
+
+# logic
+# Create accumulator variable total
+# Loop through set elements
+# Add each value to total
+
+numbers = {10, 20, 30, 40, 50}
+
+total = 0
+
+for num in numbers:
+    total += num
+
+print("Sum:", total)
