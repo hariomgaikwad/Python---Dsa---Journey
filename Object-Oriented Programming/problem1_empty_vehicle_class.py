315bb4c67edb14ef60b3ca6a1e62fe46ebb4829c
hariomgaikwad <gaikwadhariom46@gmail.com>
2026-07-02 19:38:21 +0530

    Create an empty Vehicle class

---
+++ Object-Oriented Programming/problem1_empty_vehicle_class.py
@@ -0,0 +1,12 @@
+# Exercise 1 – Define an Empty Vehicle Class
+
+# Logic
+# class Vehicle: creates a new class
+# pass is used as a placeholder because the class has no variables or methods
+# print(Vehicle) displays the class object
+
+
+class Vehicle:
+    pass
+
+print(Vehicle)
